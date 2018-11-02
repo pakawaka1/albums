02d542a4e535bb3948553adcfc04ca59dda832bc
David L Cervantes <davidlcervantes@Davids-MacBook-Pro.local>
2018-11-02 15:00:20 -0700

    feat: reusable header

--- albums/index.js
+++ albums/index.js
@@ -1,11 +1,12 @@
 // Import a library to create a component
 import React from 'react';
-import { Text, AppRegistry } from 'react-native';
+import { AppRegistry } from 'react-native';
+import Header from './src/components/header';
 
 // Create a component
 
 const App = () => (
-    <Text> Some Fucking Text </Text>
+    <Header headerText={'Albums'} />            // Same as <Header></Header>
 );
 // Render the component to device (MUST REGISTER AT LEAST ONE COMPONENT)
 
